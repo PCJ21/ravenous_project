814994647e89a080520d26cb76cdd321bb9567fa
Paulo cesar José <pcj@MacBook-Pro-de-Paulo.local>
2018-01-09 16:07:05 +0100

    last part of revenous project, not good

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './BusinessList.css';
+
 import Business from '../Business/Business';
+//import Yelp from '../../util/Yelp';
 
 class BusinessList extends React.Component{
   render(){
     return (
       <div className="BusinessList">
-        {this.props.businesses.map(business => <Business business ={business}/>)}
+        {this.props.businesses.map(business => <Business key={business.key}business ={business}/>)}
       </div>);
   }
 }
---
+++ src/util/Yelp.js
@@ -0,0 +1,29 @@
+const apiKey = "ifeoLsWsjWU0QjWTU-BAjQzxsBDBm4Li6yNT_5cQZXARgIMMB9KzegXCK-Dt1zO_b5lJonsWqNXvjXmTT9gyombGHKUFCdpuvWi7X15iqIVlvS1R99E1B90k1WFDWnYx";
+const Yelp ={
+
+  search(term,location,sortBy){
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{ headers: {Authorizarion: `Bearer ${apiKey}`}}).then(response => {
+      return response.json();
+    }
+  ).then(jsonResponse => {
+    if(jsonResponse.businesses){
+      return jsonResponse.businesses.map(business => {
+        return{
+        id: business.id,
+        imageSrc: business.image_url,
+        name: business.name,
+        address: business.location.address1,
+        city: business.location.city,
+        state: business.location.state,
+        zipCode: business.location.zipCode,
+        category: business.categories[0].title,
+        rating: business.rating,
+        reviewCount: business.reviewCount
+       }
+      });
+    }
+  });
+  }
+};
+
+export default Yelp;
