5aec0f9929b8efbfe5627b52ac2340dc7700722b
Paulo cesar José <pcj@MacBook-Pro-de-Paulo.local>
2018-01-10 17:59:05 +0100

    completed version of the prokject

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,7 +2,7 @@ const apiKey = 'pbpFDshhcX98f4lVUBLiiXpfkEikUTCeUMyaYvabQdSMUKO7sovG1fKYXo6NcM9J
 const Yelp ={
 
   search(term,location,sortBy){
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{ headers: {Authorizarion: `Bearer ${apiKey}`}}).then(response => {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{ headers: {Authorization: `Bearer ${apiKey}`}}).then(response => {
       return response.json();
     }
   ).then(jsonResponse => {
